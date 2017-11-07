78df5db30fdfe105c099a67510dcf686a37d5c55
Yang Yang Lu <lu.yang1213@gmail.com>
2017-11-06 23:14:25 -0700

    added a basic GUI

---
+++ src/GUI/SortGUI.java
@@ -0,0 +1,28 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Yang Yang Lu, John Bui, Jordan Siaha
+ *
+ * @comment SortGUI
+ */
+public class SortGUI extends JFrame {
+	
+	/* just a basic GUI to start with */
+	public static void main(String[] args) {
+		JFrame f = new SortGUI();
+		f.setVisible(true);
+	}
+	
+	// variable for window size
+	private final int WIDTH = 800;
+	private final int HEIGHT = 800;
+	
+	public SortGUI() {
+		this.setTitle("Testing");
+		this.setSize(WIDTH, HEIGHT);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		/* add Graphic view */
+	}
+}
