5a15b9c3d5cacbddda80cbc83f38e3bed7888bcb
Jordan Siaha <jordansiaha@email.arizona.edu>
2017-11-11 02:02:43 -0700

    Update Insertionsort.java

--- src/sortAlgorithms/Insertionsort.java
+++ src/sortAlgorithms/Insertionsort.java
@@ -9,7 +9,17 @@ public class Insertionsort extends SortingAlgorithm {
 
 	@Override
 	public void sort(int[] arr) {
-
+		int i = 1;
+		while(i < arr.length){
+		    int j = 1;
+		    while(j > 0 && arr[j-1] > arr[j]){
+		    	int temp = arr[j];
+		    	arr[j] = arr[j-1];
+		    	arr[j-1] = temp;
+		        j--;
+		    }
+		    i++;
+		}
 	}
 
 }
